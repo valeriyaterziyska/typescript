20b515e7764f2b1a76934fec1aa790fb845ecc40
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-04-03 16:20:07 +0300

    Create dayOfWeek.ts

---
+++ 01.Intro to TypeScript/04.dayOfWeek/dayOfWeek.ts
@@ -0,0 +1,15 @@
+const days = {
+    "Monday": "1",
+    "Tuesday": "2",
+    "Wednesday":  "3",
+    "Thursday": "4",
+    "Friday": "5",
+    "Saturday": "6",
+    "Sunday": "7"
+}
+
+function dayOfWeek(input: string) {
+    return;
+}
+
+console.log(dayOfWeek('Monday'));
