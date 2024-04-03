af2ae4d657540381050d19727d760fb0071c2dce
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-04-03 16:19:46 +0300

    update sum.ts

--- 01.Intro to TypeScript/03.sum/sum.ts
+++ 01.Intro to TypeScript/03.sum/sum.ts
@@ -1,9 +1,9 @@
-function sum(a:string, b:string) {
+function sum(a: string, b: string) {
     let aInput = Number(a);
     let bInput = Number(b);
     let sum: number = 0;
 
-    for(let i = aInput; i <= bInput; i++) {
+    for (let i = aInput; i <= bInput; i++) {
         sum += i;
     }
 
