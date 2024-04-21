fe985162267c99ec31e9ff43c5dcb499dd4ab98b
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-04-21 11:48:58 +0300

    Add towns population

---
+++ 02.AdvancedTypes/src/population.ts
@@ -0,0 +1,31 @@
+type TownPopulation = {
+  name: string;
+  population: number;
+};
+
+function townPopulation(input: string[]) {
+  const towns: TownPopulation[] = [];
+
+  input.forEach((town) => {
+    const [name, populationInput] = town.split(" <-> ");
+    const population = Number(populationInput);
+
+    let result = towns.find((el) => el.name === name); 
+
+    if (!!result) {
+      result.population += population;
+    } else {
+      towns.push({ name, population });
+    }
+  });
+
+  console.log(towns);
+}
+
+townPopulation([
+  "Istanbul <-> 100000",
+  "Honk Kong <-> 2100004",
+  "Jerusalem <-> 2352344",
+  "Mexico City <-> 23401925",
+  "Istanbul <-> 1000",
+]);
