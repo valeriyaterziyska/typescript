b79078ae98b027fd41946520695b5925f2a6a317
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-04-21 17:43:57 +0300

    Add class inheritance

---
+++ 03.OOP/src/inheritance.ts
@@ -0,0 +1,17 @@
+class Animal {
+  eat() {
+    console.log("eating...");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("barking...");
+  }
+}
+
+class Cat extends Animal {
+  meow() {
+    console.log("meowing...");
+  }
+}
